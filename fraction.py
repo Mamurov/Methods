ecba905a12f90797ca569547a74512d69715d9ca
Akyl Mamurov <akylmamurov1@gmail.com>
2021-08-30 15:48:17 +0600

    initial

---
+++ fraction.py
@@ -0,0 +1,17 @@
+class Fraction:
+    def add(self, x, y):
+        return x + y
+
+    def sub(self, x, y):
+        return x - y
+
+    def mul(self, x, y):
+        return x * y
+
+    def div(self, x, y):
+        return x // y
+
+
+example = Fraction()
+print(example.add(15, 3))
+
---
+++ line.py
@@ -0,0 +1,3 @@
+# import fraction
+# from fraction import *
+#
